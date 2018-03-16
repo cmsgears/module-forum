9b94d5796d5b01e3bf6323ac65ca737d81c1390f
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-03-16 08:27:09 +0530

    Applied quick fixes.

--- common/config/ForumGlobal.php
+++ common/config/ForumGlobal.php
@@ -1,14 +1,32 @@
 <?php
+/**
+ * This file is part of CMSGears Framework. Please view License file distributed
+ * with the source code for license details.
+ *
+ * @link https://www.cmsgears.org/
+ * @copyright Copyright (c) 2015 VulpineCode Technologies Pvt. Ltd.
+ */
+
 namespace cmsgears\forum\common\config;
 
+/**
+ * ForumGlobal defines the global constants and variables available for forum and dependent modules.
+ *
+ * @since 1.0.0
+ */
 class ForumGlobal {
-    
+
+	// System Sites ---------------------------------------------------
+
+	const SITE_FORUM			= 'forum';
+
+	// Grouping by type ------------------------------------------------
+
     // Parent Type
     const TYPE_FORUM    = 'forum';
 
     // Permissions -----------------------------------------------------
-    
-    const PERM_FORUM    = 'forum';
-}
 
-?>
+    const PERM_FORUM    = 'forum';
+
+}
