27920b1d26101bedf1a1091e5d4b6982689af48a
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2020-02-03 07:11:23 +0530

    Applied quick fixes.

--- migrations/m180222_053554_forum.php
+++ migrations/m180222_053554_forum.php
@@ -120,8 +120,11 @@ class m180222_053554_forum extends \cmsgears\core\common\base\Migration {
 			'id' => $this->bigPrimaryKey( 20 ),
 			'userId' => $this->bigInteger( 20 )->notNull(),
 			'modelId' => $this->bigInteger( 20 )->notNull(),
-			'type' => $this->smallInteger( 6 )->defaultValue( 0 ),
-			'active' => $this->boolean()->notNull()->defaultValue( false ),
+			'type' => $this->string( Yii::$app->core->mediumText )->notNull(),
+			'order' => $this->smallInteger( 6 )->defaultValue( 0 ),
+			'active' => $this->boolean()->notNull()->defaultValue( true ),
+			'pinned' => $this->boolean()->notNull()->defaultValue( false ),
+			'featured' => $this->boolean()->notNull()->defaultValue( false ),
 			'createdAt' => $this->dateTime()->notNull(),
 			'modifiedAt' => $this->dateTime(),
 			'data' => $this->mediumText()
